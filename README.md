dc38bb07af086a6a694b35d7226c3cf62f78a4d1
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-04-16 01:57:22 -0700

    Initial commit - includes default Flutter project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Starbucks
+A clone of the Starbucks app, built with Flutter.
+
+This project is not affiliated with Starbucks.
---
+++ android/app/src/main/kotlin/com/flutterclonewars/starbucks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterclonewars.starbucks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
